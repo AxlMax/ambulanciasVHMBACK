36a4d28fd6b442149df86dda7f505ff77b2aae54
diego axsel garcia <diego.garciasi@autonoma.edu.co>
2023-01-25 09:22:02 -0500

    fix : agregar respuesta cuando no encuentra el usuario

--- source/controller/login.js
+++ source/controller/login.js
@@ -8,22 +8,27 @@ const oauth = (body,res) => {
     userModel.findOne({
         email : body.email
         },(error, doc) => {
-            const equal = bcrypt.compareSync(body.passwd, doc.passwd)
 
-            if(equal){
-                
-                const secret = new TextEncoder().encode(
-                    process.env.JWT_SECRET
-                  );
-                
-                  const jwt =  new jose.SignJWT({doc})
-                    .setProtectedHeader({ alg: "HS256" })
-                    .setExpirationTime('8h')
-                    .sign(secret);
-                  
-                  jwt.then((data) => res.send('Bearer ' + data))
+            if(doc !== null){
+                const equal = bcrypt.compareSync(body.passwd, doc.passwd)
+
+                if(equal){
+                    
+                    const secret = new TextEncoder().encode(
+                        process.env.JWT_SECRET
+                      );
+                    
+                      const jwt =  new jose.SignJWT({doc})
+                        .setProtectedHeader({ alg: "HS256" })
+                        .setExpirationTime('8h')
+                        .sign(secret);
+                      
+                      jwt.then((data) => res.send('Bearer ' + data))
+                }else{
+                    res.send("no puede ingresar")
+                }
             }else{
-                res.send("no puede ingresar")
+                res.send("No existe el usuario")
             }
         })
 }
